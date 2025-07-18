cbbd94a7cecba94db0b45d9fe3b0950a705e6a76
Richi Saito <to_saitoh@hotmail.com>
2025-07-18 21:13:03 +0900

    lambda式サンプルpyの追加

---
+++ test_lambda.py
@@ -0,0 +1,13 @@
+# lambda式サンプル
+add = lambda x, y: x + y
+hello = lambda name: print(f"hello, {name}")
+
+print(add(2, 3))
+hello("taro")
+
+#mapとラムダの組み合わせ
+li = [10, 20, 30, 40]
+
+li2 = list(map(lambda x: x * 2, li))
+
+print(li2)
