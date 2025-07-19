e98c5b3ef445f2d9d89fc9e350067cb7fb11094d
Richi Saito <to_saitoh@hotmail.com>
2025-07-19 10:23:13 +0900

    listメソッドの追加（pop, remove）

--- c05_list_method.py
+++ c05_list_method.py
@@ -24,3 +24,17 @@ li_s.insert(1, "a1") # index=1に挿入
 li_s.insert(-1, "z") # この段階でのリスト末尾位置（index=-1）に挿入して、末尾にあったgは後ろに押し出される
 
 print(f"li_s = {li_s}")
+
+# リストから削除：popとremove
+pop_i = li_s.pop(-1) # リストの末尾を削除
+print(f"li_s = {li_s}")
+print(f"削除した値：{pop_i}")
+
+pop_i = li_s.pop() # indexの指定がないと、リストの末尾を削除
+print(f"li_s = {li_s}")
+print(f"削除した値：{pop_i}")
+
+li_m = [1, 2, 3, 2, 5, 2]
+li_m.remove(2) #リストの最初の2を削除
+#li_m.remove(999) # リストに無い値を指定するとエラーになる
+print(f"li_m = {li_m}")
