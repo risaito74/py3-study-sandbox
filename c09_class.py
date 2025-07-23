1f3e163a28996cde2451957d6b21c75f55566e06
Richi Saito <to_saitoh@hotmail.com>
2025-07-23 21:28:31 +0900

    9章クラスのdocstringを追記

--- c09_class.py
+++ c09_class.py
@@ -1,6 +1,8 @@
 # 9章クラス：はじめてのクラス
 
 class TestClass:
+    """ クラスの基本を理解するためのテスト用クラス """
+
     # 初期処理：新規生成されたインスタンスに対して自動的にコールされる
     def __init__(self):
         # インスタンスに属するインスタンス変数
@@ -9,6 +11,9 @@ class TestClass:
     #クラスのメソッド
     def get_num(self):
         return self.num
+
+# クラスのdocstringを表示
+print(TestClass.__doc__)
 
 # TestClassクラスのインスタンスを生成
 test = TestClass()
